0bd0bc36fe0be8316b49ffe7f8f0837e65495a15
rakshithvk19 <rakshithvk19@gmail.com>
2024-07-16 12:49:28 -0400

    feat: Added ExactSwap exercise and research questions

---
+++ Research-Questions.md
@@ -0,0 +1,6 @@
+## ExactSwap.sol
+1. In `getAmountIn()` in `UniswapV2Library.sol`, why are we adding 1 to the result of the division? What role does solidity play here?
+2. What are the gas cost implications of performing swaps directly through the pair contract versus using the Uniswap router?
+3. What are the differences in implementing exact input swaps versus exact output swaps in Uniswap V2?
+
+## ExactSwapUsingRouter.sol
---
+++ src/ExactSwap.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IERC20.sol";
+
+contract ExactSwap {
+    /**
+     *  PERFORM AN SIMPLE SWAP WITHOUT ROUTER EXERCISE
+     *
+     *  The contract has an initial balance of 1 WETH.
+     *  The challenge is to swap an exact amount of WETH for 1337 USDC token using the `swap` function
+     *  from USDC/WETH pool.
+     *
+     */
+    function performExactSwap(address pool, address weth, address usdc) public {
+        /**
+         *     swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data);
+         *
+         *     amount0Out: the amount of USDC to receive from swap.
+         *     amount1Out: the amount of WETH to receive from swap.
+         *     to: recipient address to receive the USDC tokens.
+         *     data: leave it empty.
+         */
+
+        // your code start here
+    }
+}
